191899b3965121aa617e237ab8b5ce33f1133f5e
Hima Bindu Gorre <139939774+himabindu-gorre@users.noreply.github.com>
2025-05-19 15:11:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   // base: process.env.VITE_BASE_PATH || "/Portfolio-Website"
-// })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   base: '/Portfolio-Website/', 
-//   plugins: [react()],
-// });
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
+  base: process.env.VITE_BASE_PATH || "/Portfolio",
+    resolve: {
     alias: {
       Components: path.resolve(__dirname, 'src/Components'),
     },
   },
-});
+
+})
+
+
 
